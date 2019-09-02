1604e3506ddd90ad1fbc02e7759c341cebece7ed
Alex Neyman <afn9@case.edu>
2019-09-02 16:07:52 -0400

    add array set functionality

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var space = document.createElement("div")
-space.setAttribute("id", "ds")
-space.setAttribute("style", "text-align: center")
-document.body.appendChild(space)
-var elem = document.getElementById("ds")
+// var space = document.createElement("div")
+// space.setAttribute("id", "ds")
+// space.setAttribute("style", "text-align: center")
+// document.body.appendChild(space)
+// var elem = document.getElementById("ds")
 
-var two = new Two({width: 800, height: 200}).appendTo(elem)
+// var two = new Two({width: 800, height: 200}).appendTo(elem)
 
 // var rectangle = two.makeRectangle(two.width/2,two.height/2,100,100)
 // rectangle.id = 'rect'
@@ -16,28 +16,82 @@ var two = new Two({width: 800, height: 200}).appendTo(elem)
 //   translateY: 250
 // })
 
-let i = 1;
+// let i = 1;
 
-function squareLoop() {
-  setTimeout(function () {
-    let rect = two.makeRectangle(two.width/2,two.height/2,100,100)
-    rect.id = "rect" + i;
-    two.update();
+// function squareLoop() {
+//   setTimeout(function () {
+//     let rect = two.makeRectangle(two.width/2,two.height/2,100,100)
+//     rect.id = "rect" + i
+//     two.update()
 
-    anime({
-      targets: '#rect' + i,
-      translateX: i*110,
-      translateY: 100
-    })
-    i++
-    if (i < 7) squareLoop();  
-  }, 50)
-}
-
-// function fillSquares(rect) {
-//   setTimeout(() => {
-//     rect.fill = 'rgba(0, 200, 255, 0.75)'    
-//   }, 50);
+//     anime({
+//       targets: '#rect' + i,
+//       translateX: i*110,
+//       translateY: 100
+//     })
+//     i++
+//     if (i < 7) squareLoop();  
+//   }, 50)
 // }
 
-squareLoop();
+// // function fillSquares(rect) {
+// //   setTimeout(() => {
+// //     rect.fill = 'rgba(0, 200, 255, 0.75)'    
+// //   }, 50);
+// // }
+
+// squareLoop();
+
+let button = document.getElementById("add-value")
+let newValue = document.getElementById("new-value")
+let arrayValue = document.getElementById("array-size")
+let sizeDiv = document.getElementById("array-size-div")
+let setElementDiv = document.getElementById("value-div")
+let index = document.getElementById("index")
+
+let setSize = () => {
+  let size = document.getElementById("array-size").value
+  let arrayDiv = document.getElementById("array-div")
+  for (i = 0; i < size; i++) {
+    let p = document.createElement("p")
+    p.className += "boxed"
+    p.innerText = "__"
+    arrayDiv.appendChild(p)
+    p.id = "element" + i;
+  }
+  sizeDiv.className += " hidden";
+  setElementDiv.className = "input-box"
+  document.getElementById("new-value").focus()
+}
+
+let addValue = () => {
+  let value = document.getElementById("new-value").value
+  let index = document.getElementById("index").value
+  if (value !== "") {
+    let p = document.getElementById("element" + index)
+    p.innerText = value
+    document.getElementById("new-value").value = null
+    document.getElementById("new-value").focus()
+    document.getElementById("index").value = null
+  } else {
+    
+  }
+}
+
+newValue.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addValue()
+  }
+})
+
+index.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addValue()
+  }
+})
+
+arrayValue.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    setSize()
+  }
+})
